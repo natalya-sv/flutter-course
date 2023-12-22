f3e76c6930f08d5f7f370deffed0c05c95201d67
Natalya Suslova <natalya.suslova@gmail.com>
2023-12-22 17:13:33 +0100

    add improvements for rotation, ios view

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -33,6 +33,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+        useSafeArea: true,
         isScrollControlled: true,
         context: context,
         builder: (ctx) => NewExpense(onAddExpense: _addExpense));
@@ -65,6 +66,7 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     Widget mainContent = const Center(child: Text('No expenses found'));
 
     if (_registeredExpenses.isNotEmpty) {
@@ -80,10 +82,19 @@ class _ExpensesState extends State<Expenses> {
           icon: const Icon(Icons.add),
         ),
       ]),
-      body: Column(children: [
-        Chart(expenses: _registeredExpenses),
-        Expanded(child: mainContent)
-      ]),
+      body: width < 600
+          ? Column(children: [
+              Chart(expenses: _registeredExpenses),
+              Expanded(child: mainContent)
+            ])
+          : Row(
+              children: [
+                Expanded(
+                  child: Chart(expenses: _registeredExpenses),
+                ),
+                Expanded(child: mainContent)
+              ],
+            ),
     );
   }
 }
