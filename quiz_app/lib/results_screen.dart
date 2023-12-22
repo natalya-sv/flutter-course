931730831fcf3470dba1ab5f0c508182e8275f28
Natalya Suslova <natalya.suslova@gmail.com>
2023-12-22 15:44:02 +0100

    add expense tracker app

--- quiz_app/lib/results_screen.dart
+++ quiz_app/lib/results_screen.dart
@@ -34,9 +34,9 @@ class ResultsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final summaryData = getSummaryData();
     final numTotalQuestions = questions.length;
-    final numCorrectQuestions = summaryData.where((data) {
-      return data['user_answer'] == data['correct_answer'];
-    }).length;
+    final numCorrectQuestions = summaryData
+        .where((data) => data['user_answer'] == data['correct_answer'])
+        .length;
 
     return SizedBox(
       width: double.infinity,
